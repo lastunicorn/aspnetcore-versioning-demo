47904d76aac92555b02b610cb7b91901585f3026
Alexandru Iuga <alex.iuga@gmail.com>
2025-08-11 14:16:19 +0300

    Added header versioning.

---
+++ sources/AspNetVersioningDemo.Presentation.V2/Controllers/InfoController.cs
@@ -0,0 +1,22 @@
+using Asp.Versioning;
+using DustInTheWind.AspNetVersioningDemo.Presentation.V2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DustInTheWind.AspNetVersioningDemo.Presentation.V2.Controllers;
+
+[ApiController]
+[ApiVersion("2.0")]
+[Route("info")]
+public class InfoController : ControllerBase
+{
+    [HttpGet("version")]
+    public VersionDto GetVersion()
+    {
+        return new VersionDto
+        {
+            Version = "2.0.0",
+            Date = new DateTime(2025, 07, 30, 0, 0, 0, DateTimeKind.Utc),
+            Description = "This is version 2.0 of the API, which includes additional features and improvements."
+        };
+    }
+}
